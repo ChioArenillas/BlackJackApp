7d0a2db7ee3def68bfc9c65664fc09db49eb338a
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2025-11-27 18:57:23 +0100

    changes

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 
 let firstCard = 10
-let secondCard = 12
+let secondCard = 10
 let sum = firstCard + secondCard
+
 let hasBlackJack = false
 let isAlive = true
-let message = ''
 
-if (sum < 21) {
+let message = ''
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el") 
+
+
+function startGame(){
+    sumEl.textContent = "Sum: " + sum
+    if (sum < 21) {
     message = 'Do you want to draw a new card?'
 } else if (sum === 21){
     message = 'Woww You have got Blackjac!'
     hasBlackJack = true
 } else {
-    message = 'You are out of the game! 😭'
+    message = 'You are out of the game!'
     isAlive = false
 }
+    messageEl.textContent = message
+}
 
-console.log(message)
 
