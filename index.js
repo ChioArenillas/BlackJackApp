599291bab91901a3c0a9a8b514cbf5753bc41205
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2025-11-27 19:12:51 +0100

    n

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 let firstCard = 10
 let secondCard = 10
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard 
+let thirdCard = 5
 
 let hasBlackJack = false
 let isAlive = true
@@ -9,9 +10,12 @@ let isAlive = true
 let message = ''
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el") 
+let cardsEl = document.getElementById("cards-el")
+
 
 
 function startGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEl.textContent = "Sum: " + sum
     if (sum < 21) {
     message = 'Do you want to draw a new card?'
@@ -25,4 +29,9 @@ function startGame(){
     messageEl.textContent = message
 }
 
+function newCard(){
+    let card = 5
+    sum += card
+    startGame()
+}
 
